dc564425b6d1a0ac1b09bc20edae305414648a75
javierquinterog1216 <javierquinterog1216@gmail.com>
2021-03-01 16:36:49 -0500

    basic navigation
    basic navigation

--- App.js
+++ App.js
@@ -1,21 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Navigation from "./navigations/Navigation";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello!!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Navigation></Navigation>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- navigations/Navigation.js
+++ navigations/Navigation.js
@@ -1,26 +1,24 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import {NavigationContainer} from '@react-navigation/native'
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Restaurants from '../screens/Restaurants';
-import Favorites from '../screens/Favorites';
-import TopRestaurants from '../screens/TopRestaurants';
-import Search from '../screens/Search';
-import Account from '../screens/Account';
+import Restaurants from "../screens/Restaurants";
+import Favorites from "../screens/Favorites";
+import TopRestaurants from "../screens/TopRestaurants";
+import Search from "../screens/Search";
+import Account from "../screens/Account";
 
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
-    return (
-        <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name="restaurants" component={Restaurants}/>
-                <Tab.Screen name="favorites" component={Favorites}/>
-                <Tab.Screen name="topRestaurants" component={TopRestaurants}/>
-                <Tab.Screen name="search" component={Search}/>
-                <Tab.Screen name="account" component={Account}/>
-
-            </Tab.Navigator>
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="restaurants" component={Restaurants} options={{title: "Restaurantes"}}/>
+        <Tab.Screen name="favorites" component={Favorites} options={{title: "Favoritos"}}/>
+        <Tab.Screen name="topRestaurants" component={TopRestaurants} options={{title: "Top 5"}}/>
+        <Tab.Screen name="search" component={Search} options={{title: "Buscar"}}/>
+        <Tab.Screen name="account" component={Account} options={{title: "Cuenta"}}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
