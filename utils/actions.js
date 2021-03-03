ac7885e33ed6ec87c6e2ed40da0a153c5050a3eb
javierquinterog1216 <javierquinterog1216@gmail.com>
2021-03-03 17:12:27 -0500

    add firebase
    add firebase

---
+++ utils/actions.js
@@ -0,0 +1,12 @@
+import { firebaseApp } from './firebase'
+import firebase from 'firebase'
+require('firebase/firestore')
+
+const db = firebase.firestore(firebaseApp);
+
+export const isUserLogged = () => {
+    let isLogged = false
+    firebase.auth().onAuthStateChanged((user)=>{
+        user !== null && (isLogged =  true)
+    })
+};
